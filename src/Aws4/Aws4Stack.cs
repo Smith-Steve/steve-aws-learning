edf7d02b24edd5a25f6ce6938dfdd4c8ce593999
smith-steve <steve.a.dore@outlook.com>
2022-07-02 21:53:30 -0700

    Initial commit

---
+++ src/Aws4/Aws4Stack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Aws4
+{
+    public class Aws4Stack : Stack
+    {
+        internal Aws4Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
